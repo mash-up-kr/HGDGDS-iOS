3c026e06ce79b2b77b01e92c294dedc7ca2db4a7
김남수 <are34@naver.com>
2025-05-20 13:39:42 +0900

    [#7] Feat: Swinject 패키지추가

--- Tuist/Package.swift
+++ Tuist/Package.swift
@@ -2,7 +2,8 @@
 import PackageDescription
 
 #if TUIST
-    import struct ProjectDescription.PackageSettings
+    import ProjectDescription
+    import ProjectDescriptionHelpers
 
     let packageSettings = PackageSettings(
         // Customize the product types for specific package product
@@ -12,7 +13,8 @@ import PackageDescription
             "Alamofire": .framework,
             "Nuke": .framework,
 //            "Lottie": .framework,
-            "Firebase": .framework
+            "Firebase": .framework,
+            "Swinject": .framework,
         ]
     )
 #endif
@@ -23,6 +25,7 @@ let package = Package(
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.10.2"),
 //        .package(url: "https://github.com/airbnb/lottie-ios.git", from: "4.5.1"),
         .package(url: "https://github.com/kean/Nuke.git", from: "12.8.0"),
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.12.0")
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "11.12.0"),
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1")
     ]
 )
