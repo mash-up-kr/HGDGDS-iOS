3b73766a5f036d025e7bcd94f62e97830f58bf7e
iosdevSW <iosdev.sw@gmail.com>
2025-05-26 23:46:57 +0900

    [#17] Feat: Reducerable 구현

---
+++ Projects/Core/HGCommon/Sources/Reducerable/Reducerable.swift
@@ -0,0 +1,24 @@
+//
+//  Reducerable.swift
+//  HGCommon
+//
+//  Created by iOS신상우 on 5/26/25.
+//
+
+import Foundation
+
+@dynamicMemberLookup
+public protocol Reducerable {
+    associatedtype State
+    associatedtype Action
+    
+    var state: State { get }
+    
+    func reduce(action: Action)
+}
+
+public extension Reducerable {
+    subscript<T>(dynamicMember keyPath: KeyPath<State, T>) -> T {
+           self.state[keyPath: keyPath]
+       }
+}
