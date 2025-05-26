4b4ee696ad529814e3b91ec387c649c791eccddb
hoBahk <qudgh849@gmail.com>
2025-05-27 00:47:06 +0900

    [#15] Feat: Logger 모듈 구현

---
+++ Projects/Core/HGLogger/Sources/LogTagType.swift
@@ -0,0 +1,17 @@
+//
+//  LogTag.swift
+//  HGLogger
+//
+//  Created by 박병호 on 5/26/25.
+//
+
+import Foundation
+
+public enum LogTagType: String {
+    case view
+    case viewModel
+    case repository
+    case network
+    case auth
+    case user
+}
--- Projects/Core/HGLogger/Sources/Logger.swift
+++ Projects/Core/HGLogger/Sources/Logger.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import os.log
+
+public final class Logger: Loggable {
+    private let subsystem: String
+    private let minLevel: LogLevelType
+
+    public init(
+        subsystem: String = Bundle.main.bundleIdentifier ?? "default",
+        minLevel: LogLevelType = .debug
+    ) {
+        self.subsystem = subsystem
+        self.minLevel = minLevel
+    }
+
+    public func log(
+        _ message: String,
+        level: LogLevelType,
+        tag: LogTagType? = nil,
+        file: String = #file,
+        line: Int = #line,
+        function: String = #function
+    ) {
+        #if DEBUG
+        guard level >= minLevel else {
+            return
+        }
+
+        let filename = (file as NSString).lastPathComponent
+        let category = (tag?.rawValue.capitalized) ?? "Default"
+        let logger = os.Logger(subsystem: subsystem, category: category)
+        
+        logger.log(
+            level: level.osLogType,
+            "\(level.prefix) \(filename):\(line) \(function) ▶︎ \(message, privacy: .public)"
+        )
+        #endif
+    }
+}
