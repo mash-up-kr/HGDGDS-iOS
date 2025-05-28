be9585e0259b99e71e93b60d602bb47541d0b084
hoBahk <qudgh849@gmail.com>
2025-05-29 00:15:41 +0900

    [#15] feat: 피드백 반영
    - Loggable 삭제
    - class HGLogger -> struct HGLogger
    - 오브젝트 받는 log 함수 추가
    - import os.log -> import OSLog

--- Projects/Core/HGLogger/Sources/HGLogger.swift
+++ Projects/Core/HGLogger/Sources/HGLogger.swift
@@ -6,12 +6,12 @@
 //
 
 import Foundation
-import os.log
+import OSLog
 
-public final class Logger: Loggable {
+public struct HGLogger: Sendable {
     private let subsystem: String
     private let minLevel: LogLevelType
-
+    
     public init(
         subsystem: String = Bundle.main.bundleIdentifier ?? "default",
         minLevel: LogLevelType = .debug
@@ -19,10 +19,30 @@ public final class Logger: Loggable {
         self.subsystem = subsystem
         self.minLevel = minLevel
     }
-
+    
+    public func log<T>(
+        _ object: @autoclosure () -> T,
+        level: LogLevelType = .debug,
+        tag: LogTagType? = nil,
+        file: String = #file,
+        line: Int = #line,
+        function: String = #function
+    ) {
+        let message = String(reflecting: object())
+        
+        log(
+            message,
+            level: level,
+            tag: tag,
+            file: file,
+            line: line,
+            function: function
+        )
+    }
+    
     public func log(
         _ message: String,
-        level: LogLevelType,
+        level: LogLevelType = .debug,
         tag: LogTagType? = nil,
         file: String = #file,
         line: Int = #line,
@@ -32,10 +52,10 @@ public final class Logger: Loggable {
         guard level >= minLevel else {
             return
         }
-
+        
         let filename = (file as NSString).lastPathComponent
         let category = (tag?.rawValue.capitalized) ?? "Default"
-        let logger = os.Logger(subsystem: subsystem, category: category)
+        let logger = Logger(subsystem: subsystem, category: category)
         
         logger.log(
             level: level.osLogType,
