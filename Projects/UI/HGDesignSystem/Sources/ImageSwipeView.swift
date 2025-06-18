e1716deb36ef05d92cf025cc86a17a419bb787a0
김남수 <are34@naver.com>
2025-06-19 01:25:58 +0900

    [#30] Feat: 닫기버튼 및 현재페이지 노출되는 이미지스와이프 뷰 구현

---
+++ Projects/UI/HGDesignSystem/Sources/ImageSwipeView.swift
@@ -0,0 +1,47 @@
+//
+//  ImageSwipeView.swift
+//  HGDesignSystem
+//
+//  Created by Enes on 6/19/25.
+//
+
+import SwiftUI
+
+public struct ImageSwipeView: View {
+    @Environment(\.dismiss) var dismiss
+    @State private var currentIndex: Int = 0
+    private let showIndex: Int
+    private let images: [UIImage]
+    
+    public init(showIndex: Int, images: [UIImage]) {
+        self.showIndex = showIndex
+        self.images = images
+    }
+    
+    public var body: some View {
+        ZStack {
+            HGImageViewer(currentIndex: $currentIndex, showIndex: showIndex, images: images)
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        // TODO: 닫기아이콘이 들어가야합니다
+                        Text("닫기")
+                    }
+                    Spacer()
+                    Text("\(currentIndex + 1)/\(images.count)")
+                        .foregroundStyle(.blue) // TODO: 디자인컬러 해야함 
+                }
+                .padding(.horizontal, 16)
+                .frame(height: 56)
+                Spacer()
+            }
+        }
+    }
+}
+
+#Preview {
+    ImageSwipeView(showIndex: 1, images: [.actions, .checkmark, .remove])
+}
