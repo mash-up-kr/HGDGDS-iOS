544f29f38afd64cd2cfd1bc85410a1136d817fff
iosdevSW <iosdev.sw@gmail.com>
2025-06-21 21:37:31 +0900

    [#39] Feat: PageControl 컴포넌트 추가

---
+++ Projects/UI/HGDesignSystem/Sources/Components/PageControl/HGPageControl.swift
@@ -0,0 +1,71 @@
+//
+//  HGPageControl.swift
+//  HGDesignSystem
+//
+//  Created by iOS신상우 on 6/21/25.
+//
+
+import SwiftUI
+
+
+public struct HGPageControl: View {
+    
+    private let numberOfPages: Int
+    @Binding private var currentIndex: Int
+    
+    public init(numberOfPages: Int, currentIndex: Binding<Int>) {
+        self.numberOfPages = numberOfPages
+        self._currentIndex = Binding(
+            get: {
+                min(max(currentIndex.wrappedValue, 0), numberOfPages - 1)
+            },
+            set: {
+                currentIndex.wrappedValue = min(max($0, 0), numberOfPages - 1)
+            }
+        )
+    }
+    
+    // MARK: - Constants
+    
+    private let circleSize: CGFloat = 6
+    private let spacing: CGFloat = 4
+    
+    private let selectedColor = HGColors.orange500Main.color
+    private let unselectedColor = HGColors.gray20.color
+    
+    
+    // MARK: - Body
+    
+    public var body: some View {
+        HStack(spacing: spacing) {
+            ForEach(Array(0..<numberOfPages), id: \.self) { index in
+                Circle()
+                    .fill(currentIndex == index ? selectedColor : unselectedColor)
+                    .frame(circleSize)
+                    .id(index)
+            }
+        }
+        .animation(.easeInOut(duration: 0.25), value: currentIndex)
+        
+    }
+}
+
+#Preview {
+    @Previewable @State var currentIndex: Int = 0
+    
+    VStack {
+        HGPageControl(numberOfPages: 6, currentIndex: $currentIndex)
+        
+        Button {
+            currentIndex += 1
+        } label: {
+            Text("+")
+        }
+        
+        Button {
+            currentIndex -= 1
+        } label: {
+            Text("-")
+        }
+    }
+}
