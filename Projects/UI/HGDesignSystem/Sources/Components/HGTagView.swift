9e81231f7622277f2058cba5ec90531d4d54a726
김남수 <are34@naver.com>
2025-06-21 17:08:45 +0900

    [#37] Feat: 공통 컴포넌트 태그뷰 구현

---
+++ Projects/UI/HGDesignSystem/Sources/Components/HGTagView.swift
@@ -0,0 +1,65 @@
+//
+//  HGTagView.swift
+//  HGDesignSystem
+//
+//  Created by Enes on 6/21/25.
+//
+
+import SwiftUI
+
+public struct HGTagView: View {
+    public enum Style {
+        case medium
+        case small
+    }
+    
+    public init(style: HGTagView.Style, title: String, textColor: UIAsset.Colors, backgroundColor: UIAsset.Colors) {
+        self.style = style
+        self.title = title
+        self.textColor = textColor.color
+        self.backgroundColor = backgroundColor.color
+    }
+    
+    private let style: HGTagView.Style
+    private let title: String
+    private let textColor: Color
+    private let backgroundColor: Color
+
+    public var body: some View {
+        Text(title)
+            .setTypo(font)
+            .foregroundStyle(textColor)
+            .padding(.horizontal, horizontalPadding)
+            .padding(.vertical, verticalPadding)
+            .background(backgroundColor)
+            .clipShape(Capsule())
+    }
+    
+    private var font: Typography {
+        switch style {
+        case .medium: .body_14_bold
+        case .small: .caption_12_bold
+        }
+    }
+    
+    private var horizontalPadding: CGFloat {
+        switch style {
+        case .medium: 12
+        case .small: 8
+        }
+    }
+    
+    private var verticalPadding: CGFloat {
+        switch style {
+        case .medium: 6
+        case .small: 4
+        }
+    }
+}
+
+#Preview {
+    VStack {
+        HGTagView(style: .medium, title: "하이하이", textColor: .purpleMain, backgroundColor: .purpleLight)
+        HGTagView(style: .small, title: "하이하이", textColor: .purpleMain, backgroundColor: .purpleLight)
+    }
+}
