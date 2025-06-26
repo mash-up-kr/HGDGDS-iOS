307ce11742c1044fcbda0449f1a364dc058002cd
김남수 <are34@naver.com>
2025-06-26 15:10:44 +0900

    [#45] Feat: dialog 구현

---
+++ Projects/UI/HGDesignSystem/Sources/Components/HGDialogView.swift
@@ -0,0 +1,57 @@
+//
+//  HGDialogView.swift
+//  HGDesignSystem
+//
+//  Created by Enes on 6/25/25.
+//
+
+import SwiftUI
+
+struct HGDialogView: View {
+    @Binding var isPresent: Bool
+    let title: String
+    let description: String
+    let okAction: (() -> Void)?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Text(title)
+                .setTypo(.title_20_bold)
+                .foregroundStyle(.gray95)
+                .multilineTextAlignment(.center)
+            Spacer().frame(height: 8)
+            Text(description)
+                .setTypo(.body_14_medium)
+                .foregroundStyle(.gray50)
+                .multilineTextAlignment(.center)
+            Spacer().frame(height: 32)
+            HStack {
+                HGButton(title: "취소", size: .large, variant: .subtle, isMaxWidth: true) {
+                    isPresent = false
+                }
+                HGButton(title: "네", size: .large, isMaxWidth: true) {
+                    okAction?()
+                    isPresent = false
+                }
+            }
+        }
+        .frame(width: 300)
+        .padding(.horizontal, 16)
+        .padding(.bottom, 16)
+        .padding(.top, 20)
+        .background(.white)
+        .setRadius(24)
+    }
+}
+
+#Preview(traits: .applyFont) {
+    ZStack {
+        Color.orange
+        HGDialogView(
+            isPresent: .constant(true),
+            title: "타이틀이길어집니다어디까지길어질까요2줄가나다라마바사아자차",
+            description: "내용내용내용내용가나다라마바사아자차카타차파하하하1234허허허허허허허허",
+            okAction: {}
+        )
+    }
+}
