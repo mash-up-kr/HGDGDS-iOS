b72608df1cbda0765a476faa79b65e314abc90ee
호박 <90945013+hoBahk@users.noreply.github.com>
2025-06-28 20:28:01 +0900

    Feat: ReservationCategoryType 구현 (#52)
    * Feat: ReservationCategoryType 구현
    
    * Feat: ReservationCategoryType RawValue 추가

---
+++ Projects/Domain/Reservation/Sources/ReservationCategoryType.swift
@@ -0,0 +1,16 @@
+//
+//  ReservationCategoryType.swift
+//  ReservationDomain
+//
+//  Created by 박병호 on 6/28/25.
+//
+
+import Foundation
+
+public enum ReservationCategoryType: String {
+    case restaurant = "FOOD"
+    case sports = "SPORTS"
+    case performance = "PERFORMANCE"
+    case activity = "ACTIVITY"
+    case etc = "ETC"
+}
