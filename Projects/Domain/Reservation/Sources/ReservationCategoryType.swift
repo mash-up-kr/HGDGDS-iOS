58f4e0d3c4d18523b771237886b3d93fbe031f03
hoBahk <qudgh849@gmail.com>
2025-06-28 19:29:59 +0900

    Feat: ReservationCategoryType 구현

---
+++ Projects/Domain/Reservation/Sources/ReservationCategoryType.swift
@@ -0,0 +1,16 @@
+//
+//  ReservationCategoryType.swift
+//  ReservationDomain
+//
+//  Created by 박병호 on 6/28/25.
+//
+
+import Foundation
+
+public enum ReservationCategoryType {
+    case restaurant
+    case sports
+    case concert
+    case activity
+    case etc
+}
