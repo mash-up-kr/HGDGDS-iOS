16e80c0ae7fa5e7404996643ac5d9c255ae85605
김남수 <are34@naver.com>
2025-07-03 01:03:25 +0900

    [#38] Feat: 탭바 숨김처리

--- Projects/Features/MyPage/Sources/MyPage/MyPageView.swift
+++ Projects/Features/MyPage/Sources/MyPage/MyPageView.swift
@@ -10,6 +10,7 @@ import HGDesignSystem
 
 struct MyPageView: View {
     @Environment(MyPageCoordinator.self) var coordinator
+    @Environment(HGTabViewManager.self) var tabManager
     
     var body: some View {
         ZStack {
@@ -31,6 +32,9 @@ struct MyPageView: View {
             .applyTabbarHeight(padding: 38)
             .fillMaxSize(.top)
             .overlay(alignment: .topTrailing) { settingButton }
+        }
+        .onAppear {
+            tabManager.setTabBarHidden(false)
         }
     }
     
