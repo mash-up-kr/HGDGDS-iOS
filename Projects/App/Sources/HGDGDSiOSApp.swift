095852dc4672f83a63c2158359d5375d35e56d97
김남수 <are34@naver.com>
2025-07-03 01:03:14 +0900

    [#38] Feat: 폰트적용

--- Projects/App/Sources/HGDGDSiOSApp.swift
+++ Projects/App/Sources/HGDGDSiOSApp.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import HGDesignSystem
 
 @main
 struct HGDGDSiOSApp: App {
@@ -6,6 +7,7 @@ struct HGDGDSiOSApp: App {
     
     init() {
         DependencyConfiguration.configure()
+        UIFont.registerAllFont()
     }
     
     var body: some Scene {
