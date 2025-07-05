44cd7e40cb63a1a11a04e93452dd85d5c63c91fa
김남수 <are34@naver.com>
2025-07-05 15:27:39 +0900

    [#73] Feat: Data extension JsonDecoding 옵션추가

--- Projects/Core/HGCommon/Sources/Extensions/Data+.swift
+++ Projects/Core/HGCommon/Sources/Extensions/Data+.swift
@@ -10,7 +10,7 @@ import Foundation
 public extension Data {
     var toPrettyPrintedString: String? {
         guard let object = try? JSONSerialization.jsonObject(with: self, options: []),
-              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted]),
+              let data = try? JSONSerialization.data(withJSONObject: object, options: [.prettyPrinted, .withoutEscapingSlashes]),
               let prettyPrintedString = NSString(data: data, encoding: String.Encoding.utf8.rawValue) else { return nil }
         return prettyPrintedString as String
     }
