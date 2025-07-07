5f6b28c56c8d7c9de6540409603a20f839b8b8ca
김남수 <are34@naver.com>
2025-07-07 22:31:24 +0900

    [#37] Feat: 유저 프로필 이미지 적용

--- Projects/Features/MyPage/Sources/MyPage/MyPageView.swift
+++ Projects/Features/MyPage/Sources/MyPage/MyPageView.swift
@@ -13,6 +13,18 @@ struct MyPageView: View {
     @Environment(HGTabViewManager.self) var tabManager
     @State private var viewModel: MyPageViewModel = .init()
     
+    // 디자인상 프로필이미지가 무조건 가려져야해서
+    // 이미지가 확대되는 높이만큼 간격에서 제외해서
+    // 이미지가 무조건 가려지도록 수치 계산했습니다.
+    private var imagePadding: CGFloat {
+        let originImageWidth: CGFloat = 375
+        let originImageHeight: CGFloat = 438
+        let screenWidth: CGFloat = UIWindow.current?.screen.bounds.width ?? originImageWidth
+        let newImageHeight: CGFloat = originImageHeight * screenWidth / originImageWidth
+        let diffHeight = (newImageHeight - originImageHeight) / 2
+        return 135 - diffHeight
+    }
+    
     var body: some View {
         ZStack {
             viewModel.profileType.backgroundColor
@@ -27,6 +39,12 @@ struct MyPageView: View {
                     profileType: viewModel.profileType
                 )
                 .padding(.horizontal, 16)
+                .background(alignment: .bottom) {
+                    viewModel.profileType.profileImage
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .padding(.bottom, imagePadding)
+                }
             }
             .applyTabbarHeight(padding: 38)
             .fillMaxSize(.top)
