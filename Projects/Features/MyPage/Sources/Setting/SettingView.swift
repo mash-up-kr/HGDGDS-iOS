7c2f672273611fc0c6265bee16f4bf3f3aa67039
김남수 <are34@naver.com>
2025-07-16 16:12:27 +0900

    qa 1.0.0(5) (#106)
    * [#105] Feat: FCM관련 이슈 해결
    
    * [#105] Feat: 사진레이아웃 수정
    
    * [#105] Feat: 뷰라우팅 로직 통일
    
    * [#105] Feat: 예약 참여시 새로고침 로직구현
    
    * [#105] Feat: 토큰리셋버튼 구현
    
    * [#105] Feat: 프로필이미지 통신제거 -> 로컬이미지 사용
    
    * 빌드 1.0.0 (6)

--- Projects/Features/MyPage/Sources/Setting/SettingView.swift
+++ Projects/Features/MyPage/Sources/Setting/SettingView.swift
@@ -35,6 +35,9 @@ struct SettingView: View {
             Text(viewModel.nickname)
                 .setTypo(.title_20_bold)
                 .foregroundStyle(.gray100Black)
+                .onLongPressGesture(minimumDuration: 3) {
+                    viewModel.reduce(.resetKeychain)
+                }
             Spacer()
             HGButton(title: "프로필 편집", size: .xSmall, variant: .subtle, isMaxWidth: false) {
                 coordinator.push(.editProfile)
